021ed496b99302f5d7d1bab638a3e9139ed4aa83
Matias Caceres <kcerescmh98@gmail.com>
2025-04-21 14:48:15 -0300

    feat: benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import styles from "@/style";
 import Service from "./components/Service";
+import Benefits from "./components/Benefits";
+import styles from "@/style";
 /* import { LuLoaderCircle } from "react-icons/lu";
 import { Button } from "./components/ui/button" */
 /* #0A0A0A shadcn hexa */
 
 function App() {
   return (
-    <div className="w-full bg-[#F1F5F9]" id="home">
+    <div className="w-full" id="home">
       <div className={`${styles.flexCenter} sticky top-0 z-10 backdrop-blur-3xl border-b border-neutral-700/20 `}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
@@ -24,6 +25,12 @@ function App() {
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Service />
+        </div>
+      </div>
+
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Benefits />
         </div>
       </div>
 
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,63 @@
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { FaWhatsapp } from "react-icons/fa";
+import { Button } from "./ui/button";
+import styles from '@/style'
+
+const data = [
+    {
+        id: 1,
+        icon: IoMdCheckmarkCircleOutline,
+        title: "Ahorro significativo",
+        description: "Aprovecha los precios mas bajos de Paraguay sin viajar"
+    },
+    {
+        id: 2,
+        icon: IoMdCheckmarkCircleOutline,
+        title: "Productos originales",
+        description: "Garantizamos la autenticidad de todos los productos."
+    },
+    {
+        id: 3,
+        icon: IoMdCheckmarkCircleOutline,
+        title: "Proceso transparente",
+        description: "Te mantenemos informado en cada paso del proceso."
+    }
+]
+
+const Benefits = () => {
+    return (
+        <section className={`${styles.paddingY} font-poppins`}>
+            <div className={`${styles.flexStart} lg:items-center flex-col`}>
+                <h2 className={`text-[28px] sm:text-[35px] lg:text-5xl text-center font-bold md:font-semibold text-black`}>
+                    ¿Por qué elegir nuestro servicio?
+                </h2>
+                <ul className="flex flex-col gap-8 py-8 w-full">
+                    {
+                        data.map((data) => {
+                            const Icon = data.icon
+                            return (
+                                <li className="flex items-start gap-2 border border-slate-300 rounded-sm p-4">
+                                    <Icon className="size-[24px] mt-1" />
+                                    <div>
+                                        <h3 className="font-medium text-xl">
+                                            {data.title}
+                                        </h3>
+                                        <p className="text-muted-foreground text-base">
+                                            {data.description}
+                                        </p>
+                                    </div>
+                                </li>
+                            )
+                        }
+                        )
+                    }
+                </ul>
+                <Button className={`font-semibold bg-green-600 hover:bg-green-700 md:py-6 cursor-pointer`}>
+                    Contactanos ahora <FaWhatsapp />
+                </Button>
+            </div>
+        </section>
+    )
+}
+
+export default Benefits
